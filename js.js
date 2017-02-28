25acc8b7350641ed647c5c75be2faf970ea697e5
JacobWylie <jacobwylie@gmail.com>
2017-02-28 16:11:33 +0100

    added event listener with click function

--- js.js
+++ js.js
@@ -1,4 +1,10 @@
-document.body.style.backgroundColor = "blue"
-document.getElementsByTagName('h1')[0].style.color = 'orange'
-document.getElementsByTagName('p')[0].style.color = 'yellow'
-document.getElementById("myHeading").style.backgroundColor = 'tomato'
+// document.body.style.backgroundColor = "blue"
+// document.getElementsByTagName('h1')[0].style.color = 'orange'
+// document.getElementsByTagName('p')[0].style.color = 'yellow'
+// document.getElementById("myHeading").style.backgroundColor = 'tomato'
+
+const myHeading = document.getElementById('myHeading');
+
+myHeading.addEventListener('click', () => {
+	myHeading.style.color = 'red';
+});
