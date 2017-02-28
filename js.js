bb98bf494ba123d32ee732c06286d57c598b79c0
JacobWylie <jacobwylie@gmail.com>
2017-02-28 16:41:39 +0100

    Select Elements by tag name

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
+// ***************************************
+    // SELECT ELEMENTS BY ID
+// ****************************************
+
 // document.body.style.backgroundColor = "blue"
 // document.getElementsByTagName('h1')[0].style.color = 'orange'
 // document.getElementsByTagName('p')[0].style.color = 'yellow'
@@ -5,8 +9,10 @@
 
 const myButton = document.getElementById('myButton');
 const myHeading = document.getElementById('myHeading');
+// const myHeading = document.getElementsByTagName('h1')[0];
 const myTextInput = document.getElementById('myTextInput');
 const btob = document.getElementById('btob');
+
 
 myButton.addEventListener('click', () => {
 	myHeading.style.color = myTextInput.value;
@@ -15,3 +21,41 @@ myButton.addEventListener('click', () => {
 btob.addEventListener('click', () => {
 	myHeading.style.color = "black";
 })
+
+
+// ***************************************
+     // SELECT ELEMTENTS BY TAG NAME
+// ****************************************
+
+const myList = document.getElementsByTagName('li');
+
+for (i=0; i < myList.length; i++) {
+	myList[i].style.color = 'purple';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
