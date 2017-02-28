dbd7d37d652ab6078bbc3afbfb9d7fcf71385c21
JacobWylie <jacobwylie@gmail.com>
2017-02-28 16:06:37 +0100

    linked JS file, tested document selectors

---
+++ js.js
@@ -0,0 +1,4 @@
+document.body.style.backgroundColor = "blue"
+document.getElementsByTagName('h1')[0].style.color = 'orange'
+document.getElementsByTagName('p')[0].style.color = 'yellow'
+document.getElementById("myHeading").style.backgroundColor = 'tomato'
